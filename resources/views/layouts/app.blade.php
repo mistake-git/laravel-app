d749cff4d6a368b4f78dd9d50c7dff27b2a6ddb8
EC2 Default User <ec2-user@ip-172-31-39-18.ap-northeast-1.compute.internal>
2020-05-04 06:22:16 +0000

    footerを追加しました

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,8 @@
         <main class="py-4">
             @yield('content')
       　<main class="py-4 mb-5">
+      　@component('components.footer')
+        @endcomponent
     </div>
 </body>
 </html>
