7385d6aadb3d4f75eec7cb58e4a1c3e9f8ae5476
EC2 Default User <ec2-user@ip-172-31-4-247.ap-northeast-1.compute.internal>
2020-09-02 06:21:43 +0000

    15章をレビューしました

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,8 +6,19 @@ use Illuminate\Http\Request;
 
 class DashBoardController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('dashboard.index');
+        $page = $request->page != null ? $request->page : 1;
+        $sort = $request->sort;
+        $billings = [];
+        if ($request->sort == 'month') {
+            $billings = ShoppingCart::getMonthlyBillings();
+        } else {
+            $billings = ShoppingCart::getDailyBllings();
+        }
+        $total = count($billings);
+        $paginator = new LengthAwarePaginator(array_slice($billings, ($page - 1), 15), $total, 15, $page, ['path' => 'dashboard']);
+
+        return view('dashboard.index', compact('billings', 'total', 'paginator', 'sort'));
     }
 }
